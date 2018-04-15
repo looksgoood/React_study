91f593a9dcd29ad7ac488e56c6046a5600ae0f35
Jaehyun Cho <looksgoood12@gmail.com>
2018-04-15 23:58:37 +0900

    Add enzyme test method exmple.

--- react-test_tutorial/src/components/NameForm.test.js
+++ react-test_tutorial/src/components/NameForm.test.js
@@ -1,16 +1,48 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import NameForm from './NameForm';
 
 describe('NameForm', () => {
     let component = null;
 
+    // 테스트용 onInsert function, changed 값을 바꿔준다.
+    let changed = null;
+    const onInsert = (name) => {
+        changed = name;
+    }
+
     it('renders correctly', () => {
-        component = renderer.create(<NameForm />);
+        component = shallow(<NameForm onInsert={onInsert} />);
     });
 
     it('matches snapshot', () => {
-        const tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
+        expect(component).toMatchSnapshot();
     });
+
+    describe('insert new text', () => {
+        it('has a form', () => {
+            expect(component.find('form').exists()).toBe(true);
+        });
+        it('has a input', () => {
+            expect(component.find('input').exists()).toBe(true);
+        });
+        it('simulates input change', () => {
+            const mockedEvent = {
+                target: {
+                    value: 'hello'
+                }
+            };
+            // Simulate event. second parameter is event object.
+            component.find('input').simulate('change', mockedEvent);
+            expect(component.state().name).toBe('hello');
+        });
+        it('simulates from submit', () => {
+            const mockedEvent = {
+                preventDefault: () => null // onSubmit에서 preventDefault호출하므로 막는 함수를 추가.
+            };
+            component.find('form').simulate('submit', mockedEvent);
+            expect(component.state().name).toBe('');
+            expect(changed).toBe('hello');
+        });
+    })
 });
